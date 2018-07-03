b230d6d9e89e741b1b60e3fb9c3e3107c1a0a44e
gongqiaolin <gongqiaolin@meituan.com>
2018-07-03 23:55:20 +0800

    完成判断浏览器transform是否支持

---  draggable/draggable.js
+++  draggable/draggable.js
@@ -1,1 +1,15 @@
-111
+// 使用transform控制元素移动
+
+function getTransform() {
+    var transform = '',
+    divStyle = document.createElement('div').style,
+    transformArr = ['transform','webkitTransform','MozTransform','msTransform','OTransform'],
+    i = 0,
+    len = transformArr.length;
+    for(;i < len; i++) {
+        if(transformArr[i] in divStyle) {
+            return transform = transformArr[i];
+        }
+    }
+    return transform;
+}
